7d7892fa8957e49f5a6c60b43554c239ed7444ca
Millena-Monteiro <707millenamonteiro@gmail.com>
2026-01-30 17:24:24 -0300

    style: clean up code formatting and add comments 🎀

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+/**
+ *🤖 My Tabajara Turing Machine
+ * Where arrays go in with questions and come out with answers
+ */
 function turingMachine(tape) {
   const results = [];
 
+  // We go through the "tape" 3 symbols at a time,
+  // because every operation needs a number, an operator, and another number
   for (let i = 0; i < tape.length; i += 3) {
     const num1 = tape[i];
     const operator = tape[i + 1];
@@ -10,21 +16,22 @@ function turingMachine(tape) {
 
     let result;
 
+    // This is where the logic happens:
     switch (operator) {
       case "+":
-        result = mum1 + num2;
+        result = num1 + num2; // Juntando as moedas
         break;
       case "-":
-        result = num1 - num2;
+        result = num1 - num2; // Tirando uma lasquinha
         break;
       case "*":
-        result = num1 * num2;
+        result = num1 * num2; // Multiplicando o pão 🙏🏽
         break;
       case "/":
-        result = num1 / num2;
+        result = num1 / num2; // Dividindo
         break;
       case "%":
-        result = num1 % num2;
+        result = num1 % num2; // O resto que sobrou
         break;
       default:
         result = "invalid operator";
@@ -34,6 +41,10 @@ function turingMachine(tape) {
   return results;
 }
 
-console.log(turingMachine([1, "+", 2]));
-console.log(turingMachine([1, "+", 2, 3, "*", 3]));
-console.log(turingMachine([10, "/", 2, 10, "%", 3]));
+// --- THE MOMENT OF TRUTH ---
+console.log("Simple test:", turingMachine([1, "+", 2])); // 1 + 2 = 3?
+console.log(
+  "Multiple operations together",
+  turingMachine([1, "+", 2, 3, "*", 3])
+); //the expected result: [3, 9]
+console.log("Leftovers?", turingMachine([10, "/", 2, 10, "%", 3])); // Testing the remainder of the division
